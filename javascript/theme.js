26936ea2a77cec5e471f88dea3eaa413a2d25312
LaurentArcos <laurent.arcos@gmail.com>
2022-05-13 18:27:06 +0200

    images et switch ok

---
+++ javascript/theme.js
@@ -0,0 +1,15 @@
+ function toggle() {
+    var el = document.getElementById("style1");
+    if (el.href.match("../css/professional-style.css")) {
+        el.href = "../css/manuscript-style.css";    
+    }
+    else {
+        el.href = "../css/professional-style.css";  
+    }
+
+}
+
+
+
+
+
