83e9ccf81cdea297afa0ee05f1905373554b57e1
LaurentArcos <laurent.arcos@gmail.com>
2022-05-17 23:50:18 +0200

    début manuscrit

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,15 +1,34 @@
- function toggle() {
-    var el = document.getElementById("style1");
-    if (el.href.match("../css/professional-style.css")) {
-        el.href = "../css/manuscript-style.css";    
+const themeToggle = {
+    
+init: function () {
+    document.querySelector('#button').addEventListener('click', themeToggle.handleThemeToggleBtnClick);
+},
+
+handleThemeToggleBtnClick : function (event) {
+    //console.log ('ok');
+    event.preventDefault();
+    let togglerEl = document.getElementById("theme-toggler");
+    if (togglerEl.href.match("../css/professional-style.css")) {
+        togglerEl.href = "../css/manuscript-style.css";    
     }
     else {
-        el.href = "../css/professional-style.css";  
+        togglerEl.href = "../css/professional-style.css";  
     }
+},
 
 }
 
+document.addEventListener('DOMContentLoaded', themeToggle.init);
 
 
 
-
+//* Code ci-dessous marche aussi en ajoutant onclick="toggle() sur le html du button
+//  function toggle() {
+//     var themeToggle = document.getElementById("theme-toggler");
+//     if (themeToggle.href.match("../css/professional-style.css")) {
+//         themeToggle.href = "../css/manuscript-style.css";    
+//     }
+//     else {
+//         themeToggle.href = "../css/professional-style.css";  
+//     }
+// }
