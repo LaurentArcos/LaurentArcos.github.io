fbfd50e6accc3c7561f5b3fac87979fa4055ab8f
LaurentArcos <laurent.arcos@gmail.com>
2022-05-30 17:39:45 +0200

    switch ok

---
+++ javascript/themeSwitcher.js
@@ -0,0 +1,69 @@
+
+const themeToggle = {
+
+    // init: function () {
+    //     document.querySelector('#button').addEventListener('click', themeToggle.handleThemeToggleBtnClick);
+    // },
+    
+    // handleThemeToggleBtnClick : function (event) {
+    //     //console.log ('ok');
+    //     //event.preventDefault();
+    //     let togglerEl = document.getElementById("theme-toggler");
+    //     if (togglerEl.href.match("../css/professional-style.css")) {
+    //         togglerEl.href = "../css/manuscript-style.css";    
+    //     }
+    //     else {
+    //         togglerEl.href = "../css/professional-style.css";  
+    //     }
+    // },
+    
+    // }
+    
+    // document.addEventListener('DOMContentLoaded', themeToggle.init);
+
+
+
+//* Code ci-dessous marche aussi pour le bouton unique en ajoutant onclick="toggle() sur le html du button
+//  function toggle() {
+//     var themeToggle = document.getElementById("theme-toggler");
+//     if (themeToggle.href.match("../css/professional-style.css")) {
+//         themeToggle.href = "../css/manuscript-style.css";    
+//     }
+//     else {
+//         themeToggle.href = "../css/professional-style.css";  
+//     }
+// }
+
+
+//* Code ci-dessous si je change le bouton toggle par un bouton on/off type interrupteur   
+    init: function () {
+        document.querySelector('#button').addEventListener('click', themeToggle.handleBtnClick);
+        document.querySelector('#button2').addEventListener('click', themeToggle.handleAgainBtnClick);
+    },
+
+    handleBtnClick : function (event) {
+    //console.log ('okleft');
+    //event.preventDefault();
+        let togglerEl = document.getElementById("theme-toggler");
+        if (togglerEl.href.match("../css/professional-style.css")) {
+            togglerEl.href = "../css/manuscript-style.css";    
+        }
+        else {
+            togglerEl.href = "../css/professional-style.css";  
+        }
+    },
+    
+    handleAgainBtnClick : function (event) {
+        //console.log ('okright');
+        //event.preventDefault();
+        let togglerEl = document.getElementById("theme-toggler");
+        if (togglerEl.href.match("../css/manuscript-style.css")) {
+            togglerEl.href = "../css/professional-style.css";    
+        }
+        else {
+            togglerEl.href = "../css/manuscript-style.css";  
+        }
+    },
+}
+
+document.addEventListener('DOMContentLoaded', themeToggle.init);
