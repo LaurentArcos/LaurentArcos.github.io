a859beb5372d8659f1a28ee4500d9908f12160ec
LaurentArcos <laurent.arcos@gmail.com>
2022-06-15 14:44:51 +0200

    lorem

--- javascript/themeSwitcher.js
+++ javascript/themeSwitcher.js
@@ -1,41 +1,6 @@
 
 const themeToggle = {
 
-    // init: function () {
-    //     document.querySelector('#button').addEventListener('click', themeToggle.handleThemeToggleBtnClick);
-    // },
-    
-    // handleThemeToggleBtnClick : function (event) {
-    //     //console.log ('ok');
-    //     //event.preventDefault();
-    //     let togglerEl = document.getElementById("theme-toggler");
-    //     if (togglerEl.href.match("../css/professional-style.css")) {
-    //         togglerEl.href = "../css/manuscript-style.css";    
-    //     }
-    //     else {
-    //         togglerEl.href = "../css/professional-style.css";  
-    //     }
-    // },
-    
-    // }
-    
-    // document.addEventListener('DOMContentLoaded', themeToggle.init);
-
-
-
-//* Code ci-dessous marche aussi pour le bouton unique en ajoutant onclick="toggle() sur le html du button
-//  function toggle() {
-//     var themeToggle = document.getElementById("theme-toggler");
-//     if (themeToggle.href.match("../css/professional-style.css")) {
-//         themeToggle.href = "../css/manuscript-style.css";    
-//     }
-//     else {
-//         themeToggle.href = "../css/professional-style.css";  
-//     }
-// }
-
-
-//* Code ci-dessous si je change le bouton toggle par un bouton on/off type interrupteur   
     init: function () {
         document.querySelector('#button').addEventListener('click', themeToggle.handleBtnClick);
         document.querySelector('#button2').addEventListener('click', themeToggle.handleAgainBtnClick);
@@ -67,3 +32,37 @@ const themeToggle = {
 }
 
 document.addEventListener('DOMContentLoaded', themeToggle.init);
+
+
+//* Code ci-dessous si je change pour un bouton unique
+
+    // init: function () {
+    //     document.querySelector('#button').addEventListener('click', themeToggle.handleThemeToggleBtnClick);
+    // },
+    
+    // handleThemeToggleBtnClick : function (event) {
+    //     console.log ('ok');
+    //     event.preventDefault();
+    //     let togglerEl = document.getElementById("theme-toggler");
+    //     if (togglerEl.href.match("../css/professional-style.css")) {
+    //         togglerEl.href = "../css/manuscript-style.css";    
+    //     }
+    //     else {
+    //         togglerEl.href = "../css/professional-style.css";  
+    //     }
+    // },
+    
+    // }
+    
+
+//* Code ci-dessous marche aussi pour le bouton unique en ajoutant onclick="toggle() sur le html du button
+
+//  function toggle() {
+//     var themeToggle = document.getElementById("theme-toggler");
+//     if (themeToggle.href.match("../css/professional-style.css")) {
+//         themeToggle.href = "../css/manuscript-style.css";    
+//     }
+//     else {
+//         themeToggle.href = "../css/professional-style.css";  
+//     }
+// }
